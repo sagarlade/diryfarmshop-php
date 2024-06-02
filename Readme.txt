d7352203f0066d8bfc8f2b122493f93263edcf41
Sagar Lade <81896221+sagarlade@users.noreply.github.com>
2024-06-03 04:31:11 +0530

    first commit

---
+++ Readme.txt
@@ -0,0 +1,14 @@
+How to run the Dairy Farm Shop Management System Project (DFSMS)
+
+1. Open PHPMyAdmin (http://localhost/phpmyadmin)
+
+2. Create a database with name dfsms
+
+3. Import dfsms.sql file(given inside the zip package in SQL file folder)
+
+4.Run the script http://localhost/dfsms
+
+Admin Credential
+
+Username: admin
+Password: Test@123
---
+++ dfsms/includes/footer.php
@@ -0,0 +1,9 @@
+            <div class="hk-footer-wrap container">
+                <footer class="footer">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-12">
+<p>Developed By<a href="#" class="text-dark">SL Tech</a> ©2024</p>
+                        </div>
+                    </div>
+                </footer>
+            </div>
